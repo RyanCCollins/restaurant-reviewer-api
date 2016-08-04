8ff9c98cf0b1d44e3d6f5bef6631ac7ed6a1d4d8
Ryan Collins <rccproductions@mac.com>
2016-08-04 13:59:26 -0400

    Chore: pass all user creation tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api do
     scope module: :v1 do # Scope the entire api to /api/v1
-      resources :users, only: [:show]
+      resources :users, only: [:show, :create]
     end
   end
 end
