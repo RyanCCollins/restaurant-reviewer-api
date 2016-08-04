b70256eea5037c931528fb15bd6d3de469e6b174
Ryan Collins <rccproductions@mac.com>
2016-08-04 16:22:33 -0400

    Feat: add authenticable module and tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
+    user = current_user
     if user.update(user_params)
       render json: user, status: 200, location: [:api, user]
     else
--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -2,4 +2,10 @@ module Authenticable
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
+
+  def authenticat_with_token!
+    render json: { errors: 'Not Authenticated' },
+                status: :unauthorized unless current_user.present?
+  end
+  
 end
