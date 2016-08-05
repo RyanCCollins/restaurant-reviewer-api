63685eb15964eca1048a3a300a203fd241775e16
Ryan Collins <rccproductions@mac.com>
2016-08-05 09:09:13 -0400

    Fix: error where up-to-date rake was not activated
    * Also, set the reviews to be nested under restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do # Scope the entire api to /api/v1
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
-      resources :restaurants, only: [:show, :index]
-      resources :reviews, only: [:create, :destroy, :update]
+      resources :restaurants, only: [:show, :index] do
+        resources :reviews, only: [:create, :destroy, :update]
+      end
     end
   end
 end
