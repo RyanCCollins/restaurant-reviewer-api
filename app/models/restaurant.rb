62ffd0775c22955981a133068977df6313454c89
Ryan Collins <rccproductions@mac.com>
2016-08-05 20:52:40 -0400

    Feat: add average star rating to the restaurant model and serializer

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,6 +6,7 @@ class Restaurant < ActiveRecord::Base
   alias_attribute :image, :feature_image
   def average_rating
     review_array = reviews.map(&:total_stars)
-    review_array.inject(0.0) { |sum, el| sum + el } / review_array.size
+    rating = review_array.inject(0.0) { |a, e| a + e } / review_array.size
+    rating.to_i || 0
   end
 end
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,5 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :city, :state, :zip,
-                                   :country, :phone, :website, :type, :image
+                                   :country, :phone, :website, :type, :image, :average_rating
   has_many :reviews
 end
