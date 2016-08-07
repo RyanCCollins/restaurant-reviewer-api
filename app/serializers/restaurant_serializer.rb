6745d64e20f4e4fde6d7ec0860014a8c97c5a23e
Ryan Collins <rccproductions@mac.com>
2016-08-07 10:05:11 -0400

    Feat: add hours to restaurant serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,5 +1,7 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :city, :state, :zip,
-                                   :country, :phone, :website, :type, :image, :average_rating
+  attributes :id, :name, :address,
+             :city, :state, :zip,
+             :country, :phone, :website,
+             :type, :image, :average_rating, :hours
   has_many :reviews
 end
