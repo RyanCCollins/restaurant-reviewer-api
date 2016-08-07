41097c4fa73f27ceddd981add1bb11518fe62e35
Ryan Collins <rccproductions@mac.com>
2016-08-07 09:27:48 -0400

    Feat: add restaurant hours model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,12 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews
   belongs_to :restaurant_type
+  has_many :restaurant_hours
   alias_attribute :type, :restaurant_type
   alias_attribute :city, :town
   alias_attribute :image, :feature_image
+  alias_attribute :hours, :restaurant_hours
+  
   def average_rating
     review_array = reviews.map(&:total_stars)
     rating = review_array.inject(0.0) { |a, e| a + e } / review_array.size
---
+++ spec/factories/restaurant_hours.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :restaurant_hour do
+    monday "MyString"
+    tuesday "MyString"
+    wednesday "MyString"
+    thursday "MyString"
+    friday "MyString"
+    saturday "MyString"
+    sunday "MyString"
+    restaurant nil
+  end
+end
